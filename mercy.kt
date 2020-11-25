63e6fc6f48c458949effeed36b80dbfb485e26fb
Chomsuke15 <73263053+Chomsuke15@users.noreply.github.com>
2020-11-25 18:07:53 +0800

    Update mercy.kt

--- mercy.kt
+++ mercy.kt
@@ -2,10 +2,13 @@ package com.example.helppls
 
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
 
 class mercy : AppCompatActivity() {
     var arrayLists: ArrayList<mercy.songs>? = null
@@ -23,15 +26,15 @@ class mercy : AppCompatActivity() {
         helperinos = Helperinos(applicationContext, arrayLists!!)
 
         listViews?.adapter = helperinos
-
+        registerForContextMenu(listViews)
 
     }
     private fun setData(): ArrayList<mercy.songs>? {
         val arrayList: ArrayList<songs> = ArrayList()
         arrayList.add(songs("Help me plas", "Dead student"))
-        arrayList.add(songs("Pogi ako", "Dead student"))
-        arrayList.add(songs("A cry for help", "Dead student"))
-        arrayList.add(songs("Student's Anguish in C minor", "Dead student"))
+        arrayList.add(songs("Pogi ako", "Narcissistic Student"))
+        arrayList.add(songs("A cry for help", "Crying student"))
+        arrayList.add(songs("Student's Anguish in C minor", "Classical student"))
         return arrayList!!
     }
 
@@ -81,6 +84,54 @@ class mercy : AppCompatActivity() {
 
             return view!!
         }
+    }
+    override fun onCreateOptionsMenu(smenu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.second,smenu)
+        return super.onCreateOptionsMenu(smenu)
+    }
+    override fun onCreateContextMenu(
+            smenu: ContextMenu?,
+            v: View?,
+            menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(smenu, v, menuInfo)
+        smenu!!.setHeaderTitle("Delete?")
+        smenu.add(0, v!!.id, 0, "Delete")
+        smenu.add(0, v!!.id, 1, "Refresj")
+
+    }
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val selectedItemOrder = item!!.order
+        val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        val listPosition = info.position
+        if (selectedItemOrder == 0)
+        {
+
+            var count = 0
+
+            val builder:AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle("Delete from album")
+            builder.setMessage("Are you sure you would like to remove this song from this album?")
+            builder.setPositiveButton("Yes" ,{dialog, which ->
+                arrayLists?.removeAt(listPosition)
+                helperinos?.notifyDataSetChanged()
+                dialog.dismiss()})
+            builder.setNegativeButton("No" ,{dialog, which -> dialog.dismiss()})
+            builder.setNeutralButton("Cancel", {dialog, which -> dialog.dismiss() })
+
+            val alertDialog:AlertDialog = builder.create()
+            alertDialog.show()
+
+            count ++
+
+
+        }
+        else if (selectedItemOrder == 1)
+        {
+            helperinos?.notifyDataSetChanged()
+        }
+
+        return true
     }
 }
 
